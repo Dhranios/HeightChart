2fdd940af1cec0df351b6756578d9bafd1ef4eb9
Dhranios <dhranios@gmail.com>
2026-05-03 19:17:57 +0200

    Multi-server support
    Feet <-> Meter lines button
    Owner filtering

--- script.js
+++ script.js
@@ -1,76 +1,259 @@
 const chart = document.getElementById('chart');
 let parsedEntries = [];
-fetch('entries.json')
-  .then(response => response.json())
-  .then(data => {
-    document.title = data.chart + " Height Chart";
-    data.entries.forEach(e => {
-      const entry = new Entry(
-        e.owner,
-        e.owner_name,
-        e.name,
-        e.height,
-        e.art,
-        e.align_bottom,
-        e.align_top
-      );
-      parsedEntries.push(entry);
-    });
-    parsedEntries.sort((a, b) => {
-      return b.heightCm - a.heightCm;
-    });
-    const imagePromises = parsedEntries.map(entry =>
-      waitForImage(entry.img)
-    );
-    Promise.all(imagePromises).then(() => {
-      layout();
-    });
+let serverCharts = [];
+let parsedOwners = [];
+let processingFiles = [
+  "entries.json"
+]
+let processedFiles = []
+canFilter = false;
+let ownerFilter = []
+let maxHeight = 70;
+let nextElementX = 0;
+
+let HeightMeters = false;
+
+const HeightButton = document.getElementById("HeightButton");
+
+HeightButton.addEventListener("click", () => {
+  HeightMeters = !HeightMeters;
+
+  HeightButton.textContent = HeightMeters ? "Height display: Meters" : "Height display: Feet";
+
+  if (canFilter) {
+    ChangeHeightDisplay()
+  }
 });
+
+window.addEventListener("hashchange", () => {
+  if (canFilter) {
+    updateParams();
+  }
+});
+function updateParams() {
+  const params = new URLSearchParams(window.location.search);
+  if (params.get("server")) {
+    let exists = false;
+    serverCharts.forEach(chartEntry => {
+      if (chartEntry.GetID() == params.get("server")) {
+        exists = true;
+      }
+    });
+    if (exists) {
+      serverSelector.value = params.get("server");
+      serverSelector.style.display = "none";
+    }
+    else {
+      serverSelector.style.display = "block";
+      serverSelector.selectedIndex = 0;
+    }
+  }
+  else {
+    serverSelector.style.display = "block";
+    serverSelector.selectedIndex = 0;
+  }
+  setChart(serverSelector.value);
+}
+
+const serverSelector = document.getElementById("serverSelector");
+const ownerSelector = document.getElementById("ownerSelector");
+
+serverSelector.addEventListener("change", (e) => {
+  if (canFilter) {
+    setChart(e.target.value);
+  }
+});
+ownerSelector.addEventListener("change", (e) => {
+  if (canFilter) {
+    if (ownerSelector.selectedIndex != 0) {
+      setFilter([e.target.value]);
+    } 
+    else {
+      setChart(serverSelector.value);
+    }
+  }
+});
+
+// Get all charts
+fetch("entries.json").then(response => response.json()).then(data => {
+  data.charts.forEach(c => {
+    //Get all individual charts' data
+    processingFiles.push("entries/" + c + ".json");
+    fetch("entries/" + c + ".json").then(response => response.json()).then(chartdata => {
+      console.log("Parsing chart:", c);
+      const chartEntry = new Chart(c, chartdata.chart ?? c, chartdata.owners);
+      serverCharts.push(chartEntry);
+      const serverSelectorOption = document.createElement("option");
+      serverSelectorOption.value = chartEntry.GetID();
+      serverSelectorOption.textContent = chartEntry.GetName();
+      serverSelector.appendChild(serverSelectorOption);
+      chartdata.owners.forEach(o => {
+        //If owner was not previously found, get all owner's entries
+        if (!parsedOwners.includes(o)) {
+          parsedOwners.push(o);
+          processingFiles.push("entries/" + o + "/entries.json");
+          fetch("entries/" + o + "/entries.json").then(response => response.json()).then(ownerdata => {
+            console.log("Parsing owner:", o);
+            ownerdata.entries.forEach(e => {
+              const entry = new Entry(
+                o,
+                ownerdata.owner_name,
+                e.name,
+                e.height,
+                e.art,
+                e.align_bottom,
+                e.align_top
+              );
+              parsedEntries.push(entry);
+            });
+            console.log("Parsed owner:", o);
+            processedFiles.push("entries/" + o + "/entries.json");
+          });
+        }
+      });
+      console.log("Parsed chart:", c);
+      processedFiles.push("entries/" + c + ".json");
+    });
+    processedFiles.push("entries.json");
+  });
+});
+function waitUntilProcessed(processingFiles, processedFiles) {
+  return new Promise(resolve => {
+    const interval = setInterval(() => {
+      const allDone = processingFiles.every(file =>
+        processedFiles.includes(file)
+      );
+
+      if (allDone) {
+        clearInterval(interval);
+        resolve();
+      }
+    }, 50); // check every 50ms
+  });
+}
 function waitForImage(img) {
   if (img.decode) {
     return img.decode().catch(() => {});
   }
-
   return new Promise(resolve => {
     if (img.complete) resolve();
     else img.onload = img.onerror = () => resolve();
   });
 }
-function layout() {
-  let maxAlignBottom = 0;
-  let maxHeight = 0; //floor height
-  parsedEntries.forEach(entry => {
-    maxHeight = Math.max(maxHeight, entry.heightCm + entry.alignTop);
-    maxAlignBottom = Math.max(maxAlignBottom, entry.alignBottom);
+//When all files are loaded and processed, set positions
+async function continueProcessing() {
+  await waitUntilProcessed(processingFiles, processedFiles);
+  //When all images are loaded to memory, set positions
+  Promise.all(parsedEntries.map(entry => waitForImage(entry.img))).then(() => {
+    //Sort all entries from tallest to shortest
+    parsedEntries.sort((a, b) => {
+      return b.heightCm - a.heightCm;
+    });
+    canFilter = true;
+    updateParams();
+    layout();
   });
-  const worldHeight = maxHeight + maxAlignBottom;
-  chart.style.height = `${worldHeight}px`;
-  chart.innerHTML = "";
-  let x = 0;
+}
+continueProcessing();
+function layout() {
+  console.log("LAYOUT");
+  let maxAlignBottom = 0;
+  maxHeight = 70;
   parsedEntries.forEach(entry => {
-    entry.render(chart);
-    entry.element.style.top = `${maxHeight - entry.heightCm - entry.alignTop + 9}px`; //9 px offset for some reason...
-    entry.element.style.left = `${x}px`;
-    x += entry.element.offsetWidth + 5;
+    if (!entry.hidden) {
+      maxHeight = Math.max(maxHeight, entry.heightCm + entry.alignTop + 54); //54 for the text
+      maxAlignBottom = Math.max(maxAlignBottom, entry.alignBottom);
+      console.log("NEW height: " + maxHeight + ", align: " + maxAlignBottom + ", VS align:" + entry.alignBottom);
+    }
+  });
+  console.log("FINAL height: " + maxHeight + ", align: " + maxAlignBottom);
+  chart.style.height = `${maxHeight + maxAlignBottom}px`;
+  chart.innerHTML = "";
+  nextElementX = 0;
+  parsedEntries.forEach(entry => {
+    if (!entry.hidden) {
+      entry.render(chart);
+      entry.element.style.top = `${maxHeight - entry.heightCm - entry.alignTop - 54}px`; //54 for the text
+      entry.element.style.left = `${nextElementX}px`;
+      nextElementX += entry.element.offsetWidth + 5;
+    }
   });
   let floor = document.createElement('div');
   floor.className = 'floor';
   floor.style.height = `${maxAlignBottom}px`;
-  floor.style.width = `${x}px`;
-  floor.style.top = `${maxHeight+maxAlignBottom/2}px`;
+  floor.style.width = `${nextElementX}px`;
+  floor.style.top = `${maxHeight}px`;
   chart.appendChild(floor);
-  console.log(maxAlignBottom);
-  let y = maxHeight+maxAlignBottom/2;
+  ChangeHeightDisplay();
+};
+
+function ChangeHeightDisplay() {
+  chart.querySelectorAll(".height-line").forEach(el => {
+    el.remove();
+  });
+  let y = maxHeight;
   while (y > 0) {
-    y -= 30.48; //100 for cm
+    if (HeightMeters) {
+      y -= 100;
+    }
+    else {
+      y -= 30.48;
+    }
     let heightLine = document.createElement('div');
     heightLine.className = 'height-line';
     heightLine.style.height = `2px`;
-    heightLine.style.width = `${x}px`;
+    heightLine.style.width = `${nextElementX}px`;
     heightLine.style.top = `${y}px`;
     chart.appendChild(heightLine);
   }
-};
+}
+
+function setChart(serverChart) {
+  console.log("Selected chart: " + serverChart);
+  let selectedChart;
+  serverCharts.forEach(chartEntry => {
+    if (chartEntry.GetID() == serverChart) {
+      selectedChart = chartEntry;
+    }
+  });
+  document.title = selectedChart.GetName() + " Height Chart"
+  ownerSelector.selectedIndex = 0;
+  while (ownerSelector.childElementCount > 1) {
+    ownerSelector.removeChild(ownerSelector.lastChild);
+  }
+  selectedChart.GetOwners().forEach(o => {
+    const ownerSelectorOption = document.createElement("option");
+    ownerSelectorOption.value = o;
+    ownerSelectorOption.textContent = o;
+    ownerSelector.appendChild(ownerSelectorOption);
+  });
+  setFilter(selectedChart.GetOwners())
+}
+function setFilter(owners) {
+  console.log("Selected owners: " + owners);
+  parsedEntries.forEach(entry => {
+    entry.updateVisibility(owners, owners.length == 1)
+  });
+  layout();
+}
+
+class Chart {
+  constructor(id, name, owners) {
+    this.id = id;
+    this.name = name;
+    this.owners = owners;
+  }
+  GetName() {
+    return this.name;
+  }
+  GetOwners() {
+    return this.owners;
+  }
+  GetID() {
+    return this.id;
+  }
+}
+
 
 class Entry {
   constructor(owner, ownerDisplay, name, height, art, alignBottom, alignTop) {
@@ -159,15 +342,11 @@ class Entry {
     this.cmEl.textContent = "cm: " + this.getHeightIn("cm");
   }
 
-  isOwner(ownerFilter) {
-    if (ownerFilter === "") {
+  updateVisibility(ownerFilter, singleOwner) {
+    if (ownerFilter.includes(this.ownerKey)) {
       this.hidden = false;
-      this.updateText(true);
-      return true;
-    }
-    if (ownerFilter === this.owner) {
-      this.hidden = false;
-      this.updateText(false);
+      this.element.style.display = "block";
+      this.updateText(!singleOwner);
       return true;
     }
 
